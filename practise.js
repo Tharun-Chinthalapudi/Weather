4002b577b7119e08a3160cda32a8c1dc778e3ad8
Chinthalapudi <2076138@cognizant.com>
2022-08-18 14:43:35 +0530

    practise.js is modified

--- practise.js
+++ practise.js
@@ -1,5 +1,9 @@
 function userInput() {
-    let url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/timeline?location=India,guntur,52201&aggregateHours=24&contentType=json&key=FB24MC8SH3B68KPDYQBEXYWJQ';
+    let country = document.getElementById("country").value;
+    let state = document.getElementById("state").value;
+    let district = document.getElementById("district").value;
+    console.log(country, state, district);
+    let url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/timeline?location=${country},${state},${district},52201 &aggregateHours=24&contentType=json&key=FB24MC8SH3B68KPDYQBEXYWJQ`;
     fetch(url)
         .then(response => {
 
@@ -8,6 +12,8 @@ function userInput() {
         })
         .then(data => {
             console.log(data);
+            console.log(data.latitude);
+            document.getElementById("demo").innerHTML = "Latitude:" + data.latitude + "  " + "Longitude:  " + data.longitude;
         })
         .catch()
 
